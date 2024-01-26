b81b97dca924e559c6bf0f30a59bd47aa227aaa0
Paulo Clebicar <151094973+PoloClebicar@users.noreply.github.com>
2024-01-26 14:49:32 +0000

    Day 3 Completed

---
+++ day3/part2.py
@@ -0,0 +1,50 @@
+file = open("/workspaces/AoC_2015/day3/list.txt", "r")
+
+positionList = []
+positionA = [0,0]
+positionB = [0,0]
+realSanta = True
+
+
+positionList.append([0,0])
+
+for lines in file:
+    for char in lines:
+        match char:
+            case ">":
+                if realSanta:
+                    positionA[0] += 1
+                    realSanta = False
+                else:
+                    positionB[0] += 1
+                    realSanta = True
+            case "<":
+                if realSanta:
+                    positionA[0] -= 1
+                    realSanta = False
+                else:
+                    positionB[0] -= 1
+                    realSanta = True
+            case "^":
+                if realSanta:
+                    positionA[1] += 1
+                    realSanta = False
+                else:
+                    positionB[1] += 1
+                    realSanta = True
+            case "v":
+                if realSanta:
+                    positionA[1] -= 1
+                    realSanta = False
+                else:
+                    positionB[1] -= 1
+                    realSanta = True
+            
+        if not positionA in positionList:
+            positionList.append(positionA.copy())
+        
+        if not positionB in positionList:
+            positionList.append(positionB.copy())
+
+        
+print(len(positionList))
